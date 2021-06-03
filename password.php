dd5ccf30c84c7a4fe9cab60236baae10f98dde1d
souarbh <souarbhrathore50@gmail.com>
2021-06-03 13:43:20 +0530

    updated

--- password.php
+++ password.php
@@ -1,11 +1,38 @@
 <?php
 $l=$_POST['password'];
-//print_r($_POST['password']);
-$str = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@$%^&*_";
-$str = str_shuffle($str);
-$str= substr($str,0,$l);
-?>
-<?php
+// //print_r($_POST['password']);
+// $str = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@$%^&*_";
+// if($l>=60){
+//     $str=$str*$str;
+    
+// }
+// $str = str_shuffle($str);
+// $str= substr($str,0,$l);
+// function randomPassword() {
+// echo    $alphabet = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789";
+
+//     for ($i = 0; $i < 8; $i++) {
+//         $n = rand(0, count($alphabet)-1);
+//         $pass[$i] = $alphabet[$n];
+//     }
+//     return $pass;
+// }
+// randomPassword();
+// $l=$_POST['password'];
+function randomPassword() {
+    glob($l=$_POST['password']);
+    $alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+    $pass = array(); //remember to declare $pass as an array
+    $alphaLength = strlen($alphabet) - 1; //put the length -1 in cache
+    for ($i = 0; $i < $l; $i++) {
+        $n = rand(0, $alphaLength);
+        $pass[] = $alphabet[$n];
+    }
+    echo implode($pass);
+
+    // return implode($pass); //turn the array into a string
+}
+// $value=randomPassword();
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -19,7 +46,7 @@ $str= substr($str,0,$l);
 </head>
 <body>
 <div class="text-center">
-    <h1>PASSWORD= <?=$str?></h1>
+    <h1>PASSWORD= <h4><?=$value=randomPassword();?></h4></h1>
 </div>
 <br>
 <div class=" text-center">
